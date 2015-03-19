d1efb2e81eb917bb742aa8c75175d52575119eb2
John Regner <john@johnregner.com>
2015-03-18 21:06:03 -0500

    Cleaned up documentation.
    Now uses same form as Swift Library.

--- Microfiche/Microfiche.swift
+++ Microfiche/Microfiche.swift
@@ -8,11 +8,13 @@
 
 import Foundation
 
-/** Archives a Swift Collection to a given path.
-:params: A Swift Collection (Array<T>, Dictionary<T,U>)
-:params: A valid path hopefully in the app sandbox
-:returns: true if the operation was successful, false if not
-*/
+/// Archives a Swift Collection to a given path.
+///
+/// :param: collection A Swift Collection (Array<T>, Dictionary<T,U>)
+/// :param: atPath A valid path hopefully in the app sandbox
+/// 
+/// :returns: true if the operation was successful, false if not
+///
 public func archiveCollection<T: CollectionType>(collection: T, atPath path: String) -> Bool {
     let dataArray = convertCollectionToArrayOfData(collection)
     println("DataArray: \(dataArray)")
@@ -21,11 +23,14 @@ public func archiveCollection<T: CollectionType>(collection: T, atPath path: Str
     return result
 }
 
-/** Restores a Collection which has been previously archived at a given path
-Note this method is overloaded on the return type, and uses return type type inference, be sure to assign the result to the correctly shaped collection
-:params: A path which points to an archive
-:returns: A collection of objects, or nil if an error occurs
-*/
+/// Restores a Collection which has been previously archived at a given path
+///
+/// Note this method is overloaded on the return type, and uses return type type inference, be sure to assign the result to the correctly shaped collection
+/// 
+/// :param: path A path which points to an archive
+/// 
+/// :returns: A collection of objects, or nil if an error occurs
+/// 
 public func restoreCollectionFromPath<T>(path: String) -> Array<T>?{
     let data = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as NSMutableArray?
     switch data {
@@ -33,6 +38,15 @@ public func restoreCollectionFromPath<T>(path: String) -> Array<T>?{
     case .None: return nil
     }
 }
+
+/// Restores a Collection which has been previously archived at a given path
+///
+/// Note this method is overloaded on the return type, and uses return type type inference, be sure to assign the result to the correctly shaped collection
+/// 
+/// :param: path A path which points to an archive
+/// 
+/// :returns: A collection of objects, or nil if an error occurs
+/// 
 public func restoreCollectionFromPath<T,U>(path: String) -> Dictionary<T,U>?{
     let data = NSKeyedUnarchiver.unarchiveObjectWithFile(path) as NSMutableArray?
     switch data {
@@ -41,11 +55,14 @@ public func restoreCollectionFromPath<T,U>(path: String) -> Dictionary<T,U>?{
     }
 }
 
-/**Pass in your Swift Collection before Archiving. This function returns an object which is suitable for NSKeyedArchiving
-Converts input Collection<T> into an NSMutableArray<NSData> works with Array<T> and Dictionary<T,U>
-:param: Any Swift Collection Type
-:returns: An NSMutableArray suitable for passing on to your archiver
-*/
+/// Pass in your Swift Collection before Archiving. This function returns an object which is suitable for NSKeyedArchiving
+///
+/// Converts input Collection<T> into an NSMutableArray<NSData> works with Array<T> and Dictionary<T,U>
+///
+/// :param: collection Any Swift Collection Type
+///
+/// :returns: An NSMutableArray suitable for passing on to your archiver
+///
 public func convertCollectionToArrayOfData<T: CollectionType>(collection: T) -> NSMutableArray {
     return NSMutableArray(array: map(collection){
         var mutableItem = $0
@@ -53,18 +70,22 @@ public func convertCollectionToArrayOfData<T: CollectionType>(collection: T) -> 
         })
 }
 
-/**This function takes an archive (NSMutableArray) which you will get back from an NSKeyedUnarchiver
-:params: An NSMutableArray, returned by the NSKeyedUnarchiver
-:returns: An Array of your given type. The data that was originally archived
-*/
+/// This function takes an archive (NSMutableArray) which you will get back from an NSKeyedUnarchiver
+///
+/// :param: array An NSMutableArray, returned by the NSKeyedUnarchiver
+///
+/// :returns: An Array of your given type. The data that was originally archived
+///
 public func restoreFromArchiveArray<T>( array: NSMutableArray) -> Array<T>{
     return Array<T>( map( array ) { memoryOfType(fromAnyObject: $0) } )
 }
 
-/**This function takes an archive (NSMutableArray) which you will get back from an NSKeyedUnarchiver
-:params: An NSMutableArray, returned by the NSKeyedUnarchiver
-:returns: A Dictionary of your given type. The data that was originally archived
-*/
+/// This function takes an archive (NSMutableArray) which you will get back from an NSKeyedUnarchiver
+///
+/// :param: array An NSMutableArray, returned by the NSKeyedUnarchiver
+///
+/// :returns: A Dictionary of your given type. The data that was originally archived
+///
 public func restoreFromArchiveArray<T,U>(array: NSMutableArray) -> Dictionary<T,U>{
     var results = Dictionary<T,U>()
     map(array){ item -> Void in
@@ -74,13 +95,13 @@ public func restoreFromArchiveArray<T,U>(array: NSMutableArray) -> Dictionary<T,
     return results
 }
 
-/**
-    This is a helper function. You probably should not be calling this
-    But if you do want to call it, pleas note that the Type T
-    which all the sizes and Pointer types use is inferred from the return type
-    You may need to explicitly declare a return type when using this.
-    see `restoreDictFromArchiveArray` for an example
-*/
+/// This is a helper function. You probably should not be calling this
+///
+/// But if you do want to call it, pleas note that the Type T
+/// which all the sizes and Pointer types use is inferred from the return type
+/// You may need to explicitly declare a return type when using this.
+/// see `restoreDictFromArchiveArray` for an example
+///
 private func memoryOfType<T>(fromAnyObject obj: AnyObject) -> T {
     let mutableData = obj as NSData
     var itemData = UnsafeMutablePointer<T>.alloc(sizeof(T))
