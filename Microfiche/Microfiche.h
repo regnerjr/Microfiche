0fbd99682b274111d0832888b7fe63d7f5a4704d
John Regner <john@johnregner.com>
2015-03-18 19:25:59 -0500

    Added Defines Module. Cleaned up Comments

--- Microfiche/Microfiche.h
+++ Microfiche/Microfiche.h
@@ -1,19 +1,9 @@
-//
-//  Microfiche.h
-//  Microfiche
-//
-//  Created by John Regner on 3/14/15.
-//  Copyright (c) 2015 WigglingScholars. All rights reserved.
-//
+//  Copyright (c) 2015 John Regner. All rights reserved.
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Microfiche.
 FOUNDATION_EXPORT double MicroficheVersionNumber;
 
 //! Project version string for Microfiche.
 FOUNDATION_EXPORT const unsigned char MicroficheVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Microfiche/PublicHeader.h>
-
-
